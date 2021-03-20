f7658e5af8f7d50d499e81779f7942c8b5334dec
ChigaBiga <bohdan_bochulya@ukr.net>
2021-03-20 23:38:16 +0200

    User entity created & db connection configured

---
+++ src/main/java/com/learning/hermes/persistance/entities/User.java
@@ -0,0 +1,83 @@
+package com.learning.hermes.persistance.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity(name = "users")
+public class User {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @Column(nullable = false, length = 20)
+    private String type;
+
+    @Column(nullable = false, length = 50)
+    private String firstName;
+
+    @Column(nullable = false, length = 50)
+    private String lastName;
+
+    @Column(nullable = false, length = 50)
+    private String department;
+
+    @Column(nullable = false, length = 20)
+    private int phoneNumber;
+
+    @Column(nullable = false)
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
