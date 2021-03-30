fd6c10b3c0dbe6010aa50b8a1b7c1a8cca842415
ChigaBiga <bohdan_bochulya@ukr.net>
2021-03-31 00:27:37 +0300

    request/response user details implemented.

---
+++ src/main/java/com/learning/hermes/model/request/UserDetailRequest.java
@@ -0,0 +1,14 @@
+package com.learning.hermes.rest;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+public class Req {
+
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String password;
+
+}
---
+++ src/main/java/com/learning/hermes/model/response/UserDetailsResponse.java
@@ -0,0 +1,17 @@
+package com.learning.hermes.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class UserDetailsResponse {
+
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+
+}
